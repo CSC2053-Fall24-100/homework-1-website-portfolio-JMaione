478aabe73a257d50c20eba00176bafbb4e2c837e
JMaione <jmaione@villanova.edu>
2024-09-13 13:13:27 -0400

    Delete Resources directory

--- Resources/js/script2.js
+++
@@ -1,8 +0,0 @@
-const addNameButton = document.getElementById('toggle-title');
-const heading = document.querySelector('#title h1');
-
-addNameButton.addEventListener('click', function()
-{
-    const name = prompt("Please enter your name:");
-    heading.textContent = 'Welcome, ' + name + '!'; 
-});
